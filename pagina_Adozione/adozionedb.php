b3ac7f2e2ce418d7f8c42c2e592cc4207ed128a7
John <sanjohngiangino>
2021-05-20 18:16:48 +0200

    utente update

--- pagina_Adozione/adozionedb.php
+++ pagina_Adozione/adozionedb.php
@@ -19,7 +19,7 @@ $uname=$_POST['username'];
 	$reg= "insert into adozioni(nomem,specie,username) values ('$nomem', '$specie','$uname') ";
 	pg_query($dbconn, $reg);
 	echo"Adozione registrata nel database";
-	header('location:reg_successo.html');
+	header('location:formoprofilo.html');
 
 //chiusura
         pg_free_result($result);
